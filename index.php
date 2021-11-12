fa4bc5d84025fe3bfd2ee2bf7a7dc10a7c8b46fd
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-11-12 14:35:10 +0100

    Tesztelések

--- index.php
+++ index.php
@@ -11,10 +11,42 @@ $fv = function() : array { //anonim/lambda fuggveny
 
 $t = [1, 23, 42, -4, 6, -8654, 0];
 
-$ki = array_filter($t, function($elem) {
+$ennelNagyobb = 10;
 
-    return $elem % 2 === 0;
+$ki = array_filter($t, function($elem) use($ennelNagyobb) {
+
+    return $elem > $ennelNagyobb;
 
 });
 
 var_dump($ki);
+
+$ki = array_map(function($elem) {
+
+    return abs($elem) * 100;
+
+}, $t);
+
+var_dump($ki);
+
+$ki = usort($t, function($a, $b) {
+
+    //if ($a < $b) {
+    //    
+    //    return -1;
+    //
+    //} else if($a > $b) {
+    //
+    //    return 1;
+    //
+    //} else {
+    //
+    //    return 0;
+    //    
+    //}
+
+    return $a - $b; 
+
+});
+
+var_dump($t);
