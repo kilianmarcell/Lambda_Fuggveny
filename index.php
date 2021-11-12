58a7fb43ffa53813c2683d08ef49ebaccfa5e01c
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-11-12 14:14:42 +0100

    Lista szűrése

--- index.php
+++ index.php
@@ -7,4 +7,14 @@ $fv = function() : array { //anonim/lambda fuggveny
 
 };
 
-$fv();
+//$fv();
+
+$t = [1, 23, 42, -4, 6, -8654, 0];
+
+$ki = array_filter($t, function($elem) {
+
+    return $elem % 2 === 0;
+
+});
+
+var_dump($ki);
