efe6cb36e1ee6f8fc2c6c572f494d2e392bce8b9
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-11-12 14:52:36 +0100

    HTML hozzáadva

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 
-let t = [1, 5, -64, 43, 53, -83 -2, 542];
+document.addEventListener('DOMContentLoaded', () => {
 
-let szurt = t.filter(elem => {
+    document.getElementById('gomb').addEventListener('click', () => {
 
-    return elem > 10;
+        let t = [1, 5, -64, 43, 53, -83 -2, 542];
+
+        let szurt = t.filter(elem => {
+        
+            return elem > 10;
+        
+        });
+        
+        console.log(szurt);
+        
+        let duplazott = t.map(elem => elem * 2);
+        
+        console.log(duplazott);
+        
+        t.sort((a, b) => b - a);44
+        
+        console.log(t);
+        
+    })
 
 });
-
-console.log(szurt);
-
-let duplazott = t.map(elem => elem * 2);
-
-console.log(duplazott);
-
-t.sort((a, b) => b - a);
-
-console.log(t);
